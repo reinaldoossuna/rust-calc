89f9293beff8b925fa740f682b989cafb859bfee
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2021-06-23 16:24:25 -0400

    basic layout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,58 @@
+#[allow(dead_code)]
+mod util;
+
+use crate::util::event::{Event, Events};
+use std::error::Error;
 use std::io::Write;
 use std::{collections::VecDeque, io};
+use termion::{event::Key, raw::IntoRawMode};
+use tui::{
+    backend::TermionBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders},
+    Terminal,
+};
 
-fn main() {
-    calc_loop();
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let events = Events::new();
+
+    loop {
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+            let block = Block::default().title("Block").borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            let block = Block::default().title("Block 2").borders(Borders::ALL);
+            f.render_widget(block, chunks[1]);
+        })?;
+
+        if let Event::Input(input) = events.next()? {
+            match input {
+                Key::Char('q') => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(())
 }
 
+#[allow(dead_code)]
 fn calc_loop() {
     let mut queue: VecDeque<f32> = VecDeque::new();
 
@@ -27,6 +75,7 @@ fn calc_loop() {
     }
 }
 
+#[allow(dead_code)]
 fn read_expr() -> String {
     let mut expr = String::new();
 
